67e0d255758471874468d714c5250519bc81bc3e
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2023-05-03 20:36:46 +0530

    Debouncing Search needs more optimisation

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,5 @@
 const API_KEY="AIzaSyBHohmwc4s6P5fE9P-eLtVTNeyAqAkJpxQ"
 
 export const Youtube_VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+API_KEY;
+
+export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
